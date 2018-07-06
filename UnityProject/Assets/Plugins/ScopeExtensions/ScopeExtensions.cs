5cd776be762d4c18992bfe6f4335144cbb5ece3a
Masaya Yashiro <yashims85@gmail.com>
2018-07-07 03:14:06 +0900

    feat feat(UnityProject): add samples

--- UnityProject/Assets/Plugins/ScopeExtensions/ScopeExtensions.cs
+++ UnityProject/Assets/Plugins/ScopeExtensions/ScopeExtensions.cs
@@ -4,19 +4,27 @@ namespace ScopeExtensions
 {
     public static class ScopeExtensions
     {
+        /// <summary>
+        /// 'Also' is myself pass to arguments lambda and return myself.
+        /// </summary>
         public static T Also<T>(this T self, Action<T> action)
         {
             action(self);
             return self;
         }
 
-        // Kotlin's Apply change this context to myself when run in lambda.
-        // TODO: How to execute Action<T> in T's this context?
+        /// <summary>
+        /// NOTE: Kotlin's Apply change this context to myself when run in lambda.
+        /// TODO: How to execute Action<T> in T's this context?
+        /// </summary>
         public static T Apply<T>(this T self, Action<T> action)
         {
             return self.Also(action);
         }
 
+        /// <summary>
+        /// 'Let' is myself pass to arguments lambda and returns returned value from lambda.
+        /// </summary>
         public static R Let<T, R>(this T self, Func<T, R> action)
         {
             return action(self);
@@ -27,8 +35,10 @@ namespace ScopeExtensions
             return action();
         }
 
-        // Kotlin's Run change this context to myself when run in lambda.
-        // TODO: How to execute Func<T> in T's this context?
+        /// <summary>
+        /// NOTE: Kotlin's Run change this context to myself when run in lambda.
+        /// TODO: How to execute Func<T> in T's this context?
+        /// </summary>
         public static R Run<T, R>(this T self, Func<T, R> action)
         {
             return self.Let(action);
---
+++ UnityProject/Assets/Sample/SampleGameScript.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using ScopeExtensions;
+using UnityEngine;
+
+namespace Sample
+{
+    public class SampleGameScript : MonoBehaviour
+    {
+        void Start()
+        {
+            // Case: Not for use scope functions.
+            // Need 2 variables declaration in function scope for Rotation set to Transform.
+            Transform t1 = GetComponent<Transform>();
+            Quaternion q1 = new Quaternion();
+            q1.x = 0;
+            q1.y = 0;
+            q1.x = 0;
+            t1.rotation = q1;
+
+            // Case: Use scope functions.
+            // Function scope is clean without used variables.
+            GetComponent<Transform>().Also((it) =>
+            {
+                it.rotation = new Quaternion().Also((that) =>
+                {
+                    that.x = 0;
+                    that.y = 0;
+                    that.z = 0;
+                });
+            });
+
+            // Calculation code scope is so clarity.
+            // 'Let' can use for like LINQ's 'Select'.
+            float distanceXFromGrandParent = GetComponent<Transform>().Let((it) =>
+            {
+                return it.parent.transform.localPosition.x + it.localPosition.x;
+            });
+        }
+    }
+}
